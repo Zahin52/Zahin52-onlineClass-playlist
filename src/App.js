87373774d3ede1c340ca63cf2f9c656db38d1432
naruto <naruto@pop-os.localdomain>
2020-10-20 20:43:17 +0600

    added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zahin entered here
         </a>
       </header>
     </div>
